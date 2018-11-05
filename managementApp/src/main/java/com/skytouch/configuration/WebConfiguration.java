72e08bbc44ab47cc70c15f98fbdb47763fb09261
Luis Ramirez <lramirez@nearsoft.com>
2018-11-05 09:51:12 -0700

    WebConfiguarion class and neccesary configurations are added to work with jsp

---
+++ managementApp/src/main/java/com/skytouch/configuration/WebConfiguration.java
@@ -0,0 +1,38 @@
+package com.skytouch.configuration;
+
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ServletComponentScan
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver resolver
+                = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.jsp");
+    }
+}
---
+++ managementApp/src/main/resources/WEB-INF/jsp/index.jsp
@@ -0,0 +1,8 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+
+<body>
+<h2> "Hello World JSP!" </h2>
+</body>
+
+</html>
