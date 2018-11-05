f6be11c62b8b4c7029a8ee53ff59458e9650f202
Luis Ramirez <lramirez@nearsoft.com>
2018-11-04 21:55:55 -0700

    defects in productMessageSenderTest are corrected

--- managementApp/src/test/java/com/skytouch/service/ProductMessageSenderTest.java
+++ managementApp/src/test/java/com/skytouch/service/ProductMessageSenderTest.java
@@ -1,7 +1,8 @@
 package com.skytouch.service;
 
-import com.skytouch.model.MessageServiceConfig;
+
 import com.skytouch.model.Product;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,21 +19,19 @@ import static org.mockito.Mockito.when;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProductMessageSenderTest {
-    private MessageServiceConfig messageServiceConfig;
+
 
     @Before
     public void setupData() {
-        messageServiceConfig = new MessageServiceConfig("product-exchange", "insertRoutingkey",
-                "getProductsExchange", "getProductsRoutingkey", "getProductsQueue",
-                "getProducts");
     }
 
     @Test
     public void testGetProducts() {
+        final String productName = "insertTest";
         List<Product> productList = Collections.singletonList(new Product
-                ("insertTest", "Unit Test", 1D, 1));
+                (productName, "Unit Test", 1D, 1));
         AmqpTemplate template = mock(AmqpTemplate.class);
-        when(template.convertSendAndReceive(messageServiceConfig.getProductsMessage())).thenReturn(productList);
-
+        when(template.convertSendAndReceive("getProducts")).thenReturn(productList);
+        Assert.assertEquals(productName, productList.get(0).getName());
     }
 }
