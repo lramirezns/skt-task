c46fca752e744468d4ff895780ded226fd4b798b
Luis Ramirez <lramirez@nearsoft.com>
2018-11-07 12:47:08 -0700

    defects in PR are corrected

--- common/src/main/java/com/skytouch/utility/Mapper.java
+++ common/src/main/java/com/skytouch/utility/Mapper.java
@@ -7,11 +7,12 @@ public final class Mapper{
     private static final DozerBeanMapper mapper = new DozerBeanMapper();
 
     private Mapper() {
+        throw new UnsupportedOperationException();
     }
 
     public static <T> T mapObject(Object source, Class<T> destinationClass) {
         return mapper.map(source, destinationClass);
     }
 
-
 }
+
